a0c10b11bfecc0088f47b2a26f2d7b1eb7078c02
Rolmer <rolmertelis@gmail.com>
2025-04-08 20:05:08 -0300

    first commit

---
+++ src/main/java/com/rolmer/mymock/MymockApplication.java
@@ -0,0 +1,13 @@
+package com.rolmer.mymock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rolmer/mymock/MymockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolmer.mymock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
